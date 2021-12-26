7a60173edc3de82c3c247c227a6881ae437c254f
GameModerator <jeannelee823@gmail.com>
2021-12-26 23:42:26 +0800

    deez nutz

--- src/server/index.js
+++ src/server/index.js
@@ -6,9 +6,13 @@ const ASSET_PATH = path.resolve(__dirname, '../../assets');
 
 app.use('/', express.static(ASSET_PATH));
 
+app.get('/', (_, res) => {
+    res.end('Deez Nuts.');
+});
+
 app.get('/run/:job', (req, res) => {
     cronjobs.find(cron => cron.type == req.params.job).task();
     res.end('Running');
-})
+});
 
 app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}`));
