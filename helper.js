c80c12957ed961b8b60f2250e8db8a2e50104b3b
GameModerator <jeannelee823@gmail.com>
2022-03-19 22:27:36 +0800

    Update misc helper shit

--- helper.js
+++ helper.js
@@ -2,13 +2,13 @@ const mon = 'Dragonite';
 fetch('https://unite-db.com/pokemon.json')
     .then(data => data.json())
     .then(data => data.find(({ name }) => name === mon))
-    .then(({ name, stats: {
+    .then(({ name, /* stats: {
         combat,
         resistance,
         mobility,
         scoring,
         assistance: support
-    }, tags: {
+    }, */ tags: {
         range,
         difficulty,
         role
@@ -17,11 +17,11 @@ fetch('https://unite-db.com/pokemon.json')
             [`${name.toLowerCase()}`]: {
                 evolutions: {},
                 stats: {
-                    combat,
-                    resistance,
-                    mobility,
-                    scoring,
-                    support
+                    combat: 0,
+                    resistance: 0,
+                    mobility: 0,
+                    scoring: 0,
+                    support: 0
                 },
                 tags: {
                     range,
@@ -34,18 +34,20 @@ fetch('https://unite-db.com/pokemon.json')
     })
     .then(data => window.pokemon = data);
 
-window.pokemon[mon].stats = {};
+// window.pokemon[mon].stats = {};
 
 fetch('https://unite-db.com/stats.json')
     .then(data => data.json())
-    .then(data => data.find(({ name }) => name === 'dragonite'))
+    .then(data => data.find(({ name }) => name === mon))
     .then(({ name, level }) => {
         const newLvls = level.map(({ hp, attack, defense, sp_attack, sp_defense, crit, cdr, lifesteal }) => {
             return { hp, attack, defense, sp_attack, sp_defense, crit, cdr, lifesteal };
         });
-        window.pokemon[name].stats.levels = newLvls;
+        window.pokemon[mon.toLowerCase()].stats.levels = newLvls;
     })
-    .then(() => navigator.clipboard.write(JSON.stringify(pokemon, null, 4)));
+    .then(() => console.log('done'));
+
+copy(JSON.stringify(pokemon[mon.toLowerCase()], null, 4));
 
 // Held Items
 json.map(({
