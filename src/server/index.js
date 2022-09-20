a74d771b846d8bf543075ecf887560f3ea2a2083
Azuriru <jeannelee823@gmail.com>
2022-09-20 21:52:27 +0800

    nuke server stuff i dont think its needed

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const cronjobs = require('./cronjobs');
 const app = express();
 const ASSET_PATH = path.resolve(__dirname, '../../assets');
 
@@ -8,11 +7,6 @@ app.use('/', express.static(ASSET_PATH));
 
 app.get('/', (_, res) => {
     res.end('Deez Nuts.');
-});
-
-app.get('/run/:job', (req, res) => {
-    cronjobs.find(cron => cron.type == req.params.job).task();
-    res.end('Running');
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}`));
